3aff4c5ac43e25f1a9be482b6923958129923409
Pαblo Kuhn <p4blo.kuhn@gmail.com>
2023-10-01 14:17:22 -0300

    fixed?

--- src/App.jsx
+++ src/App.jsx
@@ -13,7 +13,7 @@ function App() {
   return (
     <>
       <Routes>
-        <Route index path="/" element={<Home />} />
+        <Route path="/" element={<Home />} />
         <Route path="*" element={<Home />} />
         <Route path="/game" element={gameOn ? <Game /> : <Home />} />
         <Route path="/gameover" element={<GameOver />} />
--- src/main.jsx
+++ src/main.jsx
@@ -1,15 +1,15 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
-import { HashRouter } from "react-router-dom";
+import { BrowserRouter } from "react-router-dom";
 import { GameProvider } from "./components/context/GameContext";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-      <HashRouter>
-    <GameProvider>
-        <App/>
-    </GameProvider>
-      </HashRouter>
+    <BrowserRouter>
+      <GameProvider>
+        <App />
+      </GameProvider>
+    </BrowserRouter>
   </React.StrictMode>
 );
